e5b886732a03053ed714da529549acdf2a2333b4
Noobi <xpectuer223@gmail.com>
2020-09-15 11:10:34 +0800

    Initial Commit

---
+++ PTA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PTA
+//
+//  Created by Alex on 2020/9/15.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
